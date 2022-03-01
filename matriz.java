1eaf4d7297d3c2fdc7e443c83e0b951fd3ee1b61
Mariano Camposeco <mariano1941@outlook.es>
2022-03-01 17:36:46 -0600

    codigo agregado

---
+++ matriz.java
@@ -0,0 +1,37 @@
+/**
+ * matriz
+ */
+public class matriz {
+
+    
+public static void main(String[] args) {
+    String [][] a = new String[6][6];//i fila, j columna
+    for (int i = 0; i < a.length; i++) {
+        for (int j = 0; j < a.length; j++) {
+            a[i][j]= String.valueOf(i);
+            System.out.print(a[i][j]);
+        }
+        System.out.println("");
+    }
+    System.out.println("");
+    for (int i = 1; i < 3; i++) {//fila
+        for (int j = 1; j < 2; j++) {//columna
+            a[i][j]="#";
+            a[i][j+1]="*";
+            j++;
+        }
+    }
+    for (int i = 0; i < a.length; i++) {
+        for (int j = 0; j < a.length; j++) {
+            System.out.print(a[i][j]);
+        }
+        System.out.println("");
+    }
+    for (int i = 1; i < 3; i++) {//fila
+        for (int j = 1; j < 2; j++) {//columna
+            System.out.println(a[i][j]+"-"+a[i][j+1]);
+            j++;
+        }
+    }
+}
+}
